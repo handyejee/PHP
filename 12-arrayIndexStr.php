790e3d9792ddfebd593241fa0821c97893bb1e37
yjson <handyejee14@gmail.com>
2022-12-17 23:42:35 +0900

    12-배열인덱스(문자)-연결연산자 추가

--- 12-arrayIndexStr.php
+++ 12-arrayIndexStr.php
@@ -6,8 +6,12 @@
     $earth['nation'] = 'korea';
 
     //다수의 인덱스 넣기
-    $earth = array('nation' => 'korea', 'nation2' => 'canada', 'nation3' => 'japan');
+    $earth = array(
+        'nation' => 'korea',
+        'nation2' => 'canada',
+        'nation3' => 'japan');
 
     //earth 배열의 nation 인덱스 출력
-    echo "earth 배열의 nation 인덱스는 ". $earth['nation'], $earth['nation2'], $earth['nation3'];
+    // 연결연산자(.)를 사용해 변수 사이에 띄어쓰기를 할 수 있다
+    echo "earth 배열의 nation 인덱스는 ". $earth['nation']." ".$earth['nation2']." ".$earth['nation3'];
 ?>
