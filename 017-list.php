e581e31432a55cc99f4b1a0466dba0796046327c
yjson <handyejee14@gmail.com>
2022-12-18 22:25:33 +0900

    017-list로 배열 출력

--- 017-list.php
+++ 017-list.php
@@ -4,4 +4,12 @@
 
     list($first, $second, $third) = $fruit;
     echo $second;
+
+    /*
+    list 선언은 아래 내역을 포함하고 있다.
+    $first = $fruit[0]
+    $second = $fruit[1]
+    $third = $fruit[2]
+    */
+
 ?>
