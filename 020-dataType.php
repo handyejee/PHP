d32b1b537c86e3fb615318459312377c1f6abb2d
yjson <handyejee14@gmail.com>
2022-12-20 01:27:15 +0900

    20-getType()

---
+++ 020-dataType.php
@@ -0,0 +1,13 @@
+<?php
+    $num = 12;
+    echo "\$num의 데이터형(값 {$num})은".gettype($num);
+    echo "<br>";
+
+    $greeting = "안녕";
+    echo "\$greeting의 데이터형(값 {$greeting})은".gettype($greeting);
+    echo "<br>";
+
+    // (\ : escape문 : 특수기호를 나타내게 할 때 사용하는 기호 
+    $numStr = "121212";
+    echo "\$numStr의 데이터형(값 {$numStr})은".gettype($numStr);
+
