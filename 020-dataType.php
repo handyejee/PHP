0a5175e8ccc8ee935621b4320a740ba889695b5c
yjson <handyejee14@gmail.com>
2022-12-20 23:13:10 +0900

    20- 데이터타입 출력

--- 020-dataType.php
+++ 020-dataType.php
@@ -1,13 +1,29 @@
 <?php
     $num = 12;
-    echo "\$num의 데이터형(값 {$num})은".gettype($num);
+    echo "\$num의 데이터형(값 {$num})은 ".gettype($num);
     echo "<br>";
 
     $greeting = "안녕";
-    echo "\$greeting의 데이터형(값 {$greeting})은".gettype($greeting);
+    echo "\$greeting의 데이터형(값 {$greeting})은 ".gettype($greeting);
+    echo "<br>";
+    
+    // (\ : escape문 : 특수기호를 나타내게 할 때 사용하는 기호) 
+    $numStr = "121212";
+    echo "\$numStr의 데이터형(값 {$numStr})은 ".gettype($numStr);
     echo "<br>";
 
-    // (\ : escape문 : 특수기호를 나타내게 할 때 사용하는 기호 
-    $numStr = "121212";
-    echo "\$numStr의 데이터형(값 {$numStr})은".gettype($numStr);
+    $fruit = array();
+    echo "\$fruit의 데이터형은 ".gettype($fruit);
+    echo "<br>";
+
+    $nai = null;
+    echo "\$nai의 데이터형(값 {nai})은 ".gettype($nai);
+    echo "<br>"
+
+    $boolean = true;
+    echo "\$boolean의 데이터형(값 {boolean})은".gettype($boolean);
+?>
+
+
+
 
