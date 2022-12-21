4c5773bf8107723e07d61149401fe0a72afcc013
yjson <handyejee14@gmail.com>
2022-12-21 22:05:15 +0900

    025-switch문

---
+++ 025-1-switch.php
@@ -0,0 +1,29 @@
+<?php
+    $likeCharacter = 'choonsik';
+
+    switch ($likeCharacter) {
+        case 'mickey':
+            echo '미키';
+        break;
+
+        case 'minnie';
+            echo '미니';
+        break;
+
+        case 'choonsik';
+            echo '춘식';
+        break;
+        
+        case 'ryan';
+            echo '라이언';
+        break;
+        
+        case 'appeach';
+            echo '어피치';
+        break;
+
+        default :
+            echo '무엇도 좋아하지 않습니다.';
+        break;
+    }
+?>
