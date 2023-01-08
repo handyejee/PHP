31e00e5cd4c760ecc43954fe4cb26941fd5b6562
yjson <handyejee14@gmail.com>
2023-01-08 19:24:05 +0900

    146-form get method

---
+++ 146-form.get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get 방식 데이터 입력폼</title>
+</head>
+<body>
+    <form name='test' method='get' action='./145-get.php'>
+    나이 : <input type= 'text' name='age'/>
+    취미 : <input type= 'text' name='hobby'/>
+    <input type= 'submit' value='전송'/>
+</form>
+</body>
+</html>
