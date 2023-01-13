83e4e374d48a31fe1caa9173872dfc544fff153c
yjson <handyejee14@gmail.com>
2023-01-14 00:08:25 +0900

    문자열 끝에서부터 시작할 경우 수정

--- 041-subst.php
+++ 041-subst.php
@@ -3,7 +3,7 @@
 
     //0자리에서 시작하여 5글자를 자른 후  cutStr에 대입
     $cutStr = substr("$str", 0, 5);
-    $cutStr2 = substr("$str", -5, -1);
+    $cutStr2 = substr("$str", -4, 2);
     
     echo $cutStr."<br>";
     echo $cutStr2;
