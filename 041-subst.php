4179729c46ae9d520d11a748c5092bc33ad6c697
yjson <handyejee14@gmail.com>
2023-01-13 23:58:40 +0900

    -1일경우 변경

--- 041-subst.php
+++ 041-subst.php
@@ -3,7 +3,7 @@
 
     //0자리에서 시작하여 5글자를 자른 후  cutStr에 대입
     $cutStr = substr("$str", 0, 5);
-    $cutStr2 = substr("$str", -1, 5);
+    $cutStr2 = substr("$str", -5, -1);
     
     echo $cutStr."<br>";
     echo $cutStr2;
