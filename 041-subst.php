2f72e125439bd56fb30763bff49efca1c8df3e78
yjson <handyejee14@gmail.com>
2023-01-14 00:09:19 +0900

    문자열 끝에서 자를경우 추가

--- 041-subst.php
+++ 041-subst.php
@@ -3,6 +3,8 @@
 
     //0자리에서 시작하여 5글자를 자른 후  cutStr에 대입
     $cutStr = substr("$str", 0, 5);
+
+    //끝에서 시작할 경우 -1부터 순서대로 왼쪽 문자열은 오른쪽으로
     $cutStr2 = substr("$str", -4, 2);
     
     echo $cutStr."<br>";
