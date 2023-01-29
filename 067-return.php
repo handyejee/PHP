1434e8638ddf44409123ff46810dbd5c0c46c401
yjson <handyejee14@gmail.com>
2023-01-29 20:34:32 +0900

    return 값 확인 : 함수에 type 지정

--- 067-return.php
+++ 067-return.php
@@ -1,17 +1,16 @@
 <?php
 
-function sum($num1, $num2) 
+function sum($num1, $num2)
 {
-    $sum = $num1 + $num2;
-    return $sum;
+    return $num1 + $num2;
 
-    // 아래 코드는 작동하지 않음 
+    // 아래 코드는 작동하지 않음
     echo '함수';
 }
 
 echo sum(5, 10);
 
-$plus = sum(5, 10);
+$plus = settype(sum(5, 10), 'int');
+//php 7이상은 void 함수는 사용할 수 없어 type 을 지정해줘야 한다.
 echo '<br>';
 echo $plus + 20;
-?>
