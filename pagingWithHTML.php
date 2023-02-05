f8bffb37fa82f35bbc9b9fef565fd78e6a995fb3
yjson <handyejee14@gmail.com>
2023-02-05 23:59:54 +0900

    paging 삼항연산자 부분 추가

--- pagingWithHTML.php
+++ pagingWithHTML.php
@@ -1,5 +1,6 @@
 <?php
+$array = array('a1','a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10');
 
-$paging = [$_GET] ? : 1 ;
-$eachPage = 3;
-
+$paging = $_GET['page'] ? $_GET['page'] : 1 ;
+$eachPage = 4;
+$startnum = 
