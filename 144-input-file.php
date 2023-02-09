7955c50353e08e9102609a9ace57825ce8989fff
손예지 <yjson@daou.co.kr>
2023-02-09 17:01:22 +0900

    input type file 기능 추가

---
+++ 144-input-file.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="CP949">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>input tag -file</title>
+</head>
+<body>
+    <form name= "" method="" action="" enctype="multipart/form-data">
+        파일 : <input type ="file" name="attachedFile" >
+</body>
+</html>
