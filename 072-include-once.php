c23de2fd9eed5fdc8cf3452833a2d5cf79068a43
yjson <handyejee14@gmail.com>
2023-02-18 15:04:56 +0900

    include_once : include 를 한번만 호출

---
+++ 072-include-once.php
@@ -0,0 +1,3 @@
+<?php
+include "./070-include.php";
+include_once "./070-include.php";
---
+++ 072-include.php
@@ -0,0 +1,4 @@
+<?php
+// include 를 사용해 두번 호출 하면 호출된 값이 두번씩 노출된다.
+include "./070-include.php";
+include "./070-include.php";
