17cbad7b1283c34e031b8a971b5323731077a7fc
yjson <handyejee14@gmail.com>
2023-03-21 23:21:24 +0900

    append 연산자 예제

---
+++ urltest.php
@@ -0,0 +1,15 @@
+<?
+    if($test == "apple" && $item) {
+        $test .= "_pie";
+    }
+    
+    if($test == "apple") {
+        $testurl = "test.apple.inc";
+    }
+
+    /* 
+
+    $txt1 .= $txt2
+    append $txt2 to $txt1
+    
+    */
